9a309c564301853d874c30275703a5d88416970f
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2020-11-21 13:28:47 +0100

    Add proper auto lint and auto format
    Put the following intto your VSC `settings.json`
    ```json
        "editor.codeActionsOnSave": {
            "source.fixAll.eslint": true
          },
          "eslint.validate": ["javascript"]
    ```

--- group-task-01/.eslintrc.js
+++ group-task-01/.eslintrc.js
@@ -1,7 +1,32 @@
+// Resources
+// - https://medium.com/@ravv/setup-eslint-prettier-for-your-react-project-in-2020-2afd56b15c84
+// - https://www.digitalocean.com/community/tutorials/linting-and-formatting-with-eslint-in-vs-code
 module.exports = {
-  extends: 'react-app',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  ignorePatterns: ['/node_modules/**', '/build/**', '/out/**', '/.next/**'],
   rules: {
-    'react/react-in-jsx-scope': 'off',
-    'jsx-a11y/anchor-is-valid': 'off',
+    'no-unused-vars': ['warn', { args: 'none', argsIgnorePattern: 'req|res|next|val' }],
+    'prettier/prettier': ['error'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 }
