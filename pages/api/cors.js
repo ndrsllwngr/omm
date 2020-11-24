6bf006e0f8eb3aded6198cf19983d208c9660e78
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2020-11-24 10:52:51 +0100

    Test - Disable CORS for this request

--- pages/api/cors.js
+++ pages/api/cors.js
@@ -5,6 +5,8 @@ import initMiddleware from '@/lib/init-middleware'
 const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
   Cors({
+    // disable CORS for this request
+    origin: false,
     // Only allow requests with GET, POST and OPTIONS
     methods: ['GET', 'POST', 'OPTIONS'],
   })
