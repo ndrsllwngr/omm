22476c702406b0e6a512604c3dd1a27de54d4c34
griase94 <andy.griesbeck@gmail.com>
2020-12-09 19:59:47 +0100

    Add route to retrieve the image of a meme

---
+++ pages/api/meme/image/[id].js
@@ -0,0 +1,46 @@
+import firebase from '@/lib/firebaseNode'
+import tmp from 'tmp'
+import fs from 'fs'
+
+export default async function memeHandler(req, res) {
+  const {
+    query: { id },
+    method,
+  } = req
+
+  const db = firebase.firestore()
+  const memeCollection = db.collection('meme')
+
+  switch (method) {
+    case 'GET':
+      // Get data from your database
+      const meme = await memeCollection.doc(id).get()
+      if (meme.exists) {
+        const memeData = meme.data()
+        const storage = firebase.storage().bucket()
+
+        const tmpobj = tmp.fileSync({ postfix: '.jpg' })
+        const options = {
+          // The path to which the file should be downloaded, e.g. "./file.txt"
+          destination: tmpobj.name,
+        }
+
+        // Downloads the file
+        await storage.file(memeData.img).download(options)
+
+        //res.status(200).json(meme.data())
+        const imageBuffer = fs.readFileSync(tmpobj.name)
+        res.setHeader('Content-Type', 'image/jpg')
+        res.send(imageBuffer)
+
+        // Delete the teporary file
+        tmpobj.removeCallback()
+      } else {
+        res.status(404)
+      }
+      break
+    default:
+      res.setHeader('Allow', ['GET'])
+      res.status(405).end(`Method ${method} Not Allowed`)
+  }
+}
