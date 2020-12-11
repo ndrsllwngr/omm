9c2977cf43c3751dedd6087a14b4943e6a89ce50
Maximilian Vincent Josef Rauh <max.rawlinger@googlemail.com>
2020-12-11 20:30:36 +0100

    added changeslide function

--- components/SingleViewButton.js
+++ components/SingleViewButton.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-export const SingleViewButton = ({ className, name }) => {
-  return <button className={className}>{name}</button>
+export const SingleViewButton = ({ className, name, trigger }) => {
+  return (
+    <button className={className} onClick={trigger}>
+      {name}
+    </button>
+  )
 }
 
 SingleViewButton.propTypes = {
   className: PropTypes.string,
   name: PropTypes.string,
+  trigger: PropTypes.function,
 }
--- components/SingleViewMeme.js
+++ components/SingleViewMeme.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { SingleViewButton } from '@/components/SingleViewButton'
+import nextConfig from 'next.config'
 /*
 {memes.map((el, index) => (
 	<img key={index} src={el.url} width={el.width} height={el.height} alt={el.name} />
   ))}
 */
+
 export const SingelViewMeme = ({ memes }) => {
   const meme = memes[0]
+
+  //use callback
+  // siehe usecallback hook react
+  const changeslide = (arg) => {
+    switch (arg) {
+      case next:
+        console.log('next')
+        break
+      case prev:
+        console.log('prev')
+      default:
+        console.error('Argument not supportet.', { arg })
+    }
+  }
 
   return (
     <div className="flex flex-col mx-auto">
@@ -21,10 +37,13 @@ export const SingelViewMeme = ({ memes }) => {
       <SingleViewButton
         className="bg-blue hover:bg-blue-dark text-black font-bold py-2 px-4 rounded"
         name="prev"
+        trigger={() => changeslide(next)}
       />
+
       <SingleViewButton
         className="bg-blue hover:bg-blue-dark text-black font-bold py-2 px-4 rounded"
         name="next"
+        trigger={() => changeslide(nexxt)}
       />
       {/* Meme Info*/}
       <div className="slide_info"></div>
