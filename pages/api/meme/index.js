69abc783f278b2a2321e3574d368f09bc5daa1af
griase94 <andy.griesbeck@gmail.com>
2020-12-14 17:28:28 +0100

    Rename template_id variable

--- pages/api/meme/index.js
+++ pages/api/meme/index.js
@@ -14,20 +14,20 @@ export default async function memeHandler(req, res) {
     case 'PUT':
       // TODO make it work with a list of content
       const content = req.body.content
-      const template = req.body.template
+      const template_id = req.body.template
 
       // Create new Firebase document id
       const document = memeCollection.doc()
       const documentData = {
         id: document.id,
         content: content,
-        template: templatesCollection.doc(template),
+        template: templatesCollection.doc(template_id),
         created_at: firebase.firestore.Timestamp.now(),
         // CREATED_BY, UP and DOWN votes etc.
       }
       await document.set(documentData)
       //TODO improve download URL by dynamically adding HOST_NAME
-      documentData.template = template
+      documentData.template = template_id
       documentData.downloadURL = 'http://localhost:3000/api/meme/image/' + document.id
       res.status(200).json(documentData)
       break
