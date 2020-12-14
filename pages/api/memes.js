c41dc4d25a73a215c93e74a184e6b4848c162650
griase94 <andy.griesbeck@gmail.com>
2020-12-14 17:20:04 +0100

    Implement get all memes route & update postman collection

--- pages/api/memes.js
+++ pages/api/memes.js
@@ -1,7 +1,16 @@
-// Fake users data
-const memes = [{ id: 1 }, { id: 2 }, { id: 3 }]
+import firebase from '@/lib/firebaseNode'
 
-export default function handler(req, res) {
+const db = firebase.firestore()
+const memeCollection = db.collection('memes')
+
+export default async function handler(req, res) {
+  const memes = await memeCollection.get()
   // Get data from your database
-  res.status(200).json(memes)
+  res.status(200).json(
+    memes.docs.map((doc) => {
+      const data = doc.data()
+      data.template = data.template.id
+      return data
+    })
+  )
 }
