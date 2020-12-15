03d08754611c2d6209bffdb263c5fd09348ae265
Maximilian Vincent Josef Rauh <max.rawlinger@googlemail.com>
2020-12-15 23:35:54 +0100

    refactored reducer

--- components/SingleViewMeme.js
+++ components/SingleViewMeme.js
@@ -2,30 +2,30 @@ import React, { useReducer } from 'react'
 import PropTypes from 'prop-types'
 import { SingleViewButton } from '@/components/SingleViewButton'
 
-const reducer = (meme, arg) => {
+const reducer = (state, arg) => {
   switch (arg.type) {
     case 'next':
-      //console.log('next')
-      return memes[memes.indexOf(meme) + 1]
-    case 'prev':
-      //console.log('prev')
-      if (memes.indexOf(meme) > 0) {
-        return memes[memes.indexOf(meme) - 1]
-      } else return meme
+      return { ...state, slideIndex: state.slideIndex + 1 }
 
+    case 'prev':
+      return {
+        ...state,
+        slideIndex: state.slideIndex === 0 ? (state.slideIndex = 0) : state.slideIndex - 1,
+      }
     default:
       console.error('Argument not supportet.', arg)
   }
 }
 
 export const SingleViewMeme = ({ memes }) => {
-  const [meme, setMeme] = useReducer(reducer, memes[0])
-
-  //Why do arrow functions not work?
+  const [state, setMeme] = useReducer(reducer, { slideIndex: 0 })
+  console.log(state.slideIndex)
+  const meme = memes[state.slideIndex]
 
   return (
     <div className="flex flex-row justify-center">
       <SingleViewButton className="" name="prev" changeSlide={() => setMeme({ type: 'prev' })} />
+
       <div className="flex-col max-w-md">
         <div className="titel">{meme.name}</div>
         {/* Meme itself*/}
@@ -34,6 +34,7 @@ export const SingleViewMeme = ({ memes }) => {
         <div className="slide_info">{meme.name}</div>
       </div>
       {/* Titel of Meme*/}
+
       <SingleViewButton className="" name="next" changeSlide={() => setMeme({ type: 'next' })} />
     </div>
   )
