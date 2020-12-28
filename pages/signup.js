204fae80e8d65d42e09ea7d2b45689c72239d833
griase94 <andy.griesbeck@gmail.com>
2020-12-28 01:33:59 +0100

    Fix linting

--- pages/signup.js
+++ pages/signup.js
@@ -10,11 +10,15 @@ export default function SignUp() {
           <div className="min-h-screen flex bg-gray-200">
             <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
               <div className="text-center mt-24">
-                <h2 className="mt-6 text-center text-3xl leading-9 font-   extrabold text-gray-900">Sign up</h2>
+                <h2 className="mt-6 text-center text-3xl leading-9 font-   extrabold text-gray-900">
+                  Sign up
+                </h2>
                 <p className="mt-2 text-center text-md text-gray-600">
                   already have an account?{' '}
                   <Link href="/login">
-                    <a href="#" className="text-blue-500">Log in</a>
+                    <a href="#" className="text-blue-500">
+                      Log in
+                    </a>
                   </Link>
                 </p>
               </div>
