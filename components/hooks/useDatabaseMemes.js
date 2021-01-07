d69f6a61f9807f7dff014f1c7d176c8a670a8089
Maximilian Vincent Josef Rauh <max.rawlinger@googlemail.com>
2021-01-07 01:20:33 +0100

    Database snapshots included

--- components/hooks/useDatabaseMemes.js
+++ components/hooks/useDatabaseMemes.js
@@ -5,61 +5,76 @@ export const useDatabaseMemes = () => {
   const [Memes, setMemes] = useState([])
   const [filter, setFilter] = useState('Latest')
 
-  function changeFilter(filter) {
+  const changeFilter = (filter) => {
     setFilter(filter)
   }
+
+  const loadCreds = () => {
+    return firebase.firestore().collection('memes')
+  }
+  const setDocs = (docs) => {
+    let dbMemes = []
+    docs.forEach((doc) => {
+      dbMemes.push({ id: doc.id, ...doc.data() })
+    })
+    setMemes(dbMemes)
+  }
+  const latestMemes = () => {
+    loadCreds()
+      .orderBy('created_at')
+      .onSnapshot((docs) => {
+        setDocs(docs)
+      })
+  }
+  const twoMemes = () => {
+    loadCreds()
+      .orderBy('created_at')
+      .limit(2)
+      .onSnapshot((docs) => {
+        setDocs(docs)
+      })
+  }
+
+  // async function getLatestMemes() {
+  //   const docs = await loadCreds().orderBy('created_at').get()
+  //   let dbMemes = []
+  //   for (let i = 0; i < docs.size; i++) {
+  //     const doc = docs.docs[i]
+  //     dbMemes.push({ id: doc.id, ...doc.data() })
+  //   }
+  //   return dbMemes
+  // }
+
+  // async function getOldestMemes() {
+  //   const docs = await loadCreds().orderBy('created_at').limit(2).get()
+  //   let dbMemes = []
+  //   for (let i = 0; i < docs.size; i++) {
+  //     const doc = docs.docs[i]
+  //     dbMemes.push({ id: doc.id, ...doc.data() })
+  //   }
+  //   return dbMemes
+  // }
+
+  // function resolveMemes(cb) {
+  //   cb.then((res) => {
+  //     setMemes(res)
+  //   }).catch(function (error) {
+  //     console.log({ error })
+  //   })
+  // }
 
   useEffect(() => {
     // TODO subscribe to get updates
     // TODO add paginationa
     switch (filter) {
       case 'Latest':
-        async function getLatestMemes() {
-          const docs = await firebase.firestore().collection('memes').orderBy('created_at').get()
-          let dbMemes = []
-          for (let i = 0; i < docs.size; i++) {
-            const doc = docs.docs[i]
-            dbMemes.push({ id: doc.id, ...doc.data() })
-          }
-          return dbMemes
-        }
-
-        getLatestMemes()
-          .then((res) => {
-            setMemes(res)
-          })
-          .catch(function (error) {
-            console.log({ error })
-          })
+        //resolveMemes(getLatestMemes())
+        latestMemes()
         break
       case 'Votes':
-        async function getOldestMemes() {
-          const docs = await firebase
-            .firestore()
-            .collection('memes')
-            .orderBy('created_at')
-            .limit(2)
-            .get()
-          let dbMemes = []
-          for (let i = 0; i < docs.size; i++) {
-            const doc = docs.docs[i]
-            dbMemes.push({ id: doc.id, ...doc.data() })
-          }
-          return dbMemes
-        }
-
-        getOldestMemes()
-          .then((res) => {
-            setMemes(res)
-          })
-          .catch(function (error) {
-            console.log({ error })
-          })
+        //resolveMemes(getOldestMemes())
+        twoMemes()
         break
-      // case 'Views':
-      //   setFilter(filter)
-      // //
-
       default:
         console.log('Unsupported case')
     }
