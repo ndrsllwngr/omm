b0996dc621e8b8a7d6dd17c0f904d7bc138339ef
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2021-01-13 00:44:55 +0100

    Add favicons for dev and prod

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,37 @@ class NextSite extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href={`/assets/favicon/${
+              process.env.NEXT_PUBLIC_DEV_MODE === 'true' ? 'dev/' : ''
+            }apple-touch-icon.png`}
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href={`/assets/favicon/${
+              process.env.NEXT_PUBLIC_DEV_MODE === 'true' ? 'dev/' : ''
+            }favicon-32x32.png`}
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href={`/assets/favicon/${
+              process.env.NEXT_PUBLIC_DEV_MODE === 'true' ? 'dev/' : ''
+            }favicon-16x16.png`}
+          />
+          <link
+            rel="manifest"
+            href={`/assets/favicon/${
+              process.env.NEXT_PUBLIC_DEV_MODE === 'true' ? 'dev/' : ''
+            }site.webmanifest`}
+          />
+        </Head>
         <body className="antialiased font-sans bg-gray-200">
           <Main />
           <NextScript />
