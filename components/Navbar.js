755fd8d2cf63a74619de62e7c901b6e1c77e243f
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2021-01-15 22:32:54 +0100

    Fix lint errors

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Link from 'next/link'
 import { ThemeToggle } from '@/components/ThemeToggle'
 import { NavButtons } from '@/components/NavButtons'
