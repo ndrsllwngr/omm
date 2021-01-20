8840bc1cfa24863f17f73f667c51a8f10ca8ae3b
Maximilian Vincent Josef Rauh <max.rawlinger@googlemail.com>
2021-01-20 23:25:19 +0100

    Button styles

--- components/SocialShareButtons.js
+++ components/SocialShareButtons.js
@@ -17,31 +17,43 @@ import PropTypes from 'prop-types'
 //https://www.npmjs.com/package/react-share
 export const SocialShareButtons = ({ id }) => {
   const url = `${window.location.origin}` + '/meme/' + `${id}`
-  const style = 'w-8 h-8 mx-1 rounded-full bg-custom-green'
-  const color = 'custom.color-green'
+  const style = 'w-8 h-8 mx-1 rounded-full bg-custom-gray fill-current text-custom-green'
   return (
     <div className="flex flex-row justify-end my-2">
       <FacebookShareButton url={url}>
         <FacebookIcon
           className={style}
-          iconFillColor={'transparent'}
+          iconFillColor={'inherit'}
           bgStyle={{ fill: 'transparent' }}
         />
       </FacebookShareButton>
+
       <TwitterShareButton url={url}>
-        <TwitterIcon className={style} />
+        <TwitterIcon
+          className={style}
+          iconFillColor={'inherit'}
+          bgStyle={{ fill: 'transparent' }}
+        />
       </TwitterShareButton>
       <EmailShareButton url={url}>
-        <EmailIcon className={style} />
+        <EmailIcon className={style} iconFillColor={'inherit'} bgStyle={{ fill: 'transparent' }} />
       </EmailShareButton>
       <RedditShareButton url={url}>
-        <RedditIcon className={style} />
+        <RedditIcon className={style} iconFillColor={'inherit'} bgStyle={{ fill: 'transparent' }} />
       </RedditShareButton>
       <TelegramShareButton url={url}>
-        <TelegramIcon className={style} />
+        <TelegramIcon
+          className={style}
+          iconFillColor={'inherit'}
+          bgStyle={{ fill: 'transparent' }}
+        />
       </TelegramShareButton>
       <WhatsappShareButton url={url}>
-        <WhatsappIcon className={style} />
+        <WhatsappIcon
+          className={style}
+          iconFillColor={'inherit'}
+          bgStyle={{ fill: 'transparent' }}
+        />
       </WhatsappShareButton>
     </div>
   )
