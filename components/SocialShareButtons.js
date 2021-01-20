83f957be917e5ca3e585f7d8ea023a0fcc6fbb09
Maximilian Vincent Josef Rauh <max.rawlinger@googlemail.com>
2021-01-20 16:55:12 +0100

    Cleanup

--- components/SocialShareButtons.js
+++ components/SocialShareButtons.js
@@ -13,14 +13,10 @@ import {
   WhatsappShareButton,
   WhatsappIcon,
 } from 'react-share'
-import { useRouter } from 'next/router'
 import PropTypes from 'prop-types'
-import { memeType } from '@/components/types/types'
-import { SingleMeme } from '@/components/SingleMeme'
 //https://www.npmjs.com/package/react-share
 export const SocialShareButtons = ({ id }) => {
   const url = `${window.location.origin}` + '/meme/' + `${id}`
-
   const style = 'w-8 h-8 mx-1 rounded-full'
   return (
     <div className="flex flex-row justify-end my-2">
