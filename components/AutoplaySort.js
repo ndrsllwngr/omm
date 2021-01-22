12a93f007c9e70a94608c32d47f7573acfb1b70e
Maximilian Vincent Josef Rauh <max.rawlinger@googlemail.com>
2021-01-23 00:17:02 +0100

    Additional reduced style

--- components/AutoplaySort.js
+++ components/AutoplaySort.js
@@ -39,7 +39,6 @@ export const AutoplaySort = () => {
           ) : (
             <IoReturnDownForwardOutline size={28} className="fill-current text-custom-green" />
           )}
-          {'Autoplay'}
           {/*          <svg
             className="-mr-1 ml-2 h-5 w-5"
             xmlns="http://www.w3.org/2000/svg"
