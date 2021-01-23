f86d788e9848071ffb526ba25c2af6c9cf99cf66
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2021-01-23 14:33:02 +0100

    Fix autoplay stopping after 1-2 memes in ORDERED mode

--- components/context/singlememeContext.js
+++ components/context/singlememeContext.js
@@ -4,12 +4,16 @@ import { useImmer } from 'use-immer'
 import { useRouter } from 'next/router'
 
 export const SingleMemeContext = createContext({})
+export const SingleMemeLoadingContext = createContext({})
 
 export const SingleMemeProvider = ({ children }) => {
   const router = useRouter()
   const [currentMeme, updateCurrent] = useImmer(null)
   const [prevMeme, setPrev] = useState(null)
   const [nextMeme, setNext] = useState(null)
+  const [prevIsLoading, setPrevIsLoading] = useState(false)
+  const [currentIsLoading, setCurrentIsLoading] = useState(false)
+  const [nextIsLoading, setNextIsLoading] = useState(false)
 
   useEffect(() => {
     if (router.pathname !== '/meme/[id]') {
@@ -28,9 +32,28 @@ export const SingleMemeProvider = ({ children }) => {
     <SingleMemeContext.Provider
       value={{ currentMeme, updateCurrent, prevMeme, setPrev, nextMeme, setNext }}
     >
-      {children}
+      <SingleMemeLoadingContext.Provider
+        value={{
+          prevIsLoading,
+          nextIsLoading,
+          setPrevIsLoading,
+          setNextIsLoading,
+          currentIsLoading,
+          setCurrentIsLoading,
+        }}
+      >
+        {children}
+      </SingleMemeLoadingContext.Provider>
     </SingleMemeContext.Provider>
   )
+}
+
+export const useSingleMemeLoadingContext = () => {
+  const context = useContext(SingleMemeLoadingContext)
+  if (!context) {
+    throw new Error(`useSingleMemeLoadingContext must be used within a SingleMemeProvider`)
+  }
+  return context
 }
 
 export const useSingleMemeContext = () => {
