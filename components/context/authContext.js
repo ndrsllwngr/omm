7dde59b7dd503236485ed56dc99489ad9818513e
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2021-01-27 00:01:59 +0100

    Improve auth context
    Co-authored-by: Andy Griesbeck <andy.griesbeck@gmail.com>
    Co-authored-by: Maximilian Rauh <max.rawlinger@googlemail.com>

--- components/context/authContext.js
+++ components/context/authContext.js
@@ -1,19 +1,16 @@
 import React, { useState, useEffect, createContext, useContext } from 'react'
 import PropTypes from 'prop-types'
-import firebase from '@/lib/firebase'
-import { FIRESTORE_COLLECTION } from '@/lib/constants'
-import { useRouter } from 'next/router'
 import UnauthorizedPage from '@/pages/403'
 import * as Realm from 'realm-web'
+import { useRouter } from 'next/router'
 
 export const AuthContext = createContext({ user: null })
 
 export default function AuthContextComp({ children }) {
   const [user, setUser] = useState(null)
+  const router = useRouter()
   const APP_ID = process.env.NEXT_PUBLIC_MONGODB_REALM_ID
   const app = new Realm.App(APP_ID)
-
-  //const router = useRouter()
 
   const createLogin = ({ email, password }) => {
     return app.emailPasswordAuth.registerUser(email, password)
@@ -31,68 +28,23 @@ export default function AuthContextComp({ children }) {
 
   // TODO use anon creds if logged out
   const signOut = () => {
-    return app.currentUser.logOut().then((user) => {
-      setUser(null)
-      return user
-    })
+    return app.currentUser
+      .logOut()
+      .then((_user) => {
+        setUser('ANON')
+        return app.logIn(Realm.Credentials.anonymous())
+      })
+      .then(() => {
+        router.push('/')
+      })
   }
 
+  //Handle auth state changes
   useEffect(() => {
-    console.log({ user })
-  }, [user])
-
-  // const getUserAdditionalData = (user) => {
-  //   return firebase
-  //     .firestore()
-  //     .collection(FIRESTORE_COLLECTION.USERS)
-  //     .doc(user.uid)
-  //     .get()
-  //     .then((userData) => {
-  //       console.debug(
-  //         'FIRESTORE_COLLECTION.USERS',
-  //         'READ',
-  //         'AuthContextComp',
-  //         'getUserAdditionalData'
-  //       )
-  //       if (userData.data()) {
-  //         setUser(userData.data())
-  //       }
-  //     })
-  // }
-  //
-  // //Handle auth state changes
-  // useEffect(() => {
-  //   const handleAuthStateChanged = (user) => {
-  //     console.log('handleAuthStateChanged: ' + user)
-  //     //setUser(user)
-  //     if (user) {
-  //       getUserAdditionalData(user)
-  //     } else {
-  //       setUser(null)
-  //     }
-  //   }
-  //
-  //   const unsub = firebase.auth().onAuthStateChanged(handleAuthStateChanged)
-  //
-  //   return () => unsub()
-  // }, [])
-
-  // Handle updates of the user document
-  // useEffect(() => {
-  //   if (user && user.uid) {
-  //     // Subscribe to user document on mount
-  //     const unsubscribe = db
-  //       .collection(FIRESTORE_COLLECTION.USERS)
-  //       .doc(user.uid)
-  //       .onSnapshot((doc) => {
-  //         console.debug(`FIRESTORE_COLLECTION.USERS`, 'READ', 'AuthContextComp', 'useEffect')
-  //         setUser(doc.data())
-  //       })
-  //     return () => unsubscribe()
-  //   }
-  //   // TODO Evaluate the dependencies of this useEffect.
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [])
+    if (!user) {
+      setUser(app.currentUser)
+    }
+  }, [setUser, user])
 
   return (
     <AuthContext.Provider value={{ user, signOut, loginEmailPassword, createLogin }}>
