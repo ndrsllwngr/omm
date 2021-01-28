df53a14a7076c614ec32d2917457ba1b146fdc48
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2021-01-28 12:56:41 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,7 @@ import { HtmlHead } from '@/components/HtmlHead'
 import { Navbar } from '@/components/Navbar'
 import { Sort } from '@/components/Sort'
 import { SingleMeme } from '@/components/SingleMeme'
-import InfiniteScroll from 'react-infinite-scroller'
-import { useDatabaseMemes } from '@/components/hooks/useDatabaseMemes'
+// import InfiniteScroll from 'react-infinite-scroller'
 import { gql, NetworkStatus, useQuery } from '@apollo/client'
 import { useSortContext } from '@/components/context/viewsContext'
 import { SORT } from '@/lib/constants'
@@ -26,7 +25,7 @@ const LandingPage = () => {
 
 export const ALL_PUBLIC_MEMES_QUERY = gql`
   query getAllPublicMemes($sortBy: MemeSortByInput) {
-    memes(query: { visibility: "PUBLIC" }, sortBy: $sortBy, limit: 4) {
+    memes(query: { visibility: "PUBLIC" }, sortBy: $sortBy) {
       _id
       createdAt
       createdBy {
@@ -91,16 +90,6 @@ const LandingPageInner = ({ sort }) => {
   })
   const loadingMorePosts = networkStatus === NetworkStatus.fetchMore
 
-  // const loadMorePosts = () => {
-  //   fetchMore({
-  //     variables: {
-  //       skip: data.length,
-  //     },
-  //   })
-  //     .then((r) => console.log(r))
-  //     .catch((e) => console.error(e))
-  // }
-
   useEffect(() => {
     console.log({ data, error, loading })
   }, [data, error, loading])
@@ -108,21 +97,16 @@ const LandingPageInner = ({ sort }) => {
   if (error) return <div>Error loading posts.</div>
   if (loading && !loadingMorePosts) return <div>Loading</div>
 
-  // const { allMemes, _allMemesMeta } = data
-  // const areMorePosts = allMemes?.length < _allMemesMeta?.count
+  // TODO add InfiniteScroll or pagination
   return (
-    <>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-16 gap-x-2 px-4 py-4 sm:px-6 sm:py-12 lg:px-0">
       {data &&
         data.memes.map((meme, index) => (
           <div key={index} className="place-self-center justify-self-center">
             <SingleMeme meme={meme} enableLink={true} />
           </div>
         ))}
-
-      {/*     <button onClick={() => loadMorePosts()} disabled={loadingMorePosts}>
-        {loadingMorePosts ? 'Loading...' : 'Show More'}
-      </button>*/}
-    </>
+    </div>
   )
 }
 
