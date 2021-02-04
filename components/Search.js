6e28e99ae3d2696b0ebfb20a6077e3d6fca6aa25
Maximilian Vincent Josef Rauh <max.rawlinger@googlemail.com>
2021-02-04 15:02:19 +0100

    Debug searchfield

--- components/Search.js
+++ components/Search.js
@@ -43,30 +43,63 @@ export const FEED_SEARCH_QUERY = gql`
 `
 //https://github.com/howtographql/react-apollo/blob/master/src/components/Search.js
 export const Search = () => {
-  const inputFieldRef = useRef(null)
-  const [isActive, setIsActive] = useDetectOutsideClick(inputFieldRef, false)
+  const searchContainerRef = useRef(null)
+  const [isActive, setIsActive] = useDetectOutsideClick(searchContainerRef, false)
   const [executeSearch, { data }] = useLazyQuery(FEED_SEARCH_QUERY)
+  const [searchFilter, setSearchFilter] = useState('')
+
+  useEffect(() => {
+    if (isActive) {
+      console.log('Searchfield active')
+    } else {
+      console.log('Searchfield INACTIVE')
+    }
+    if (data) {
+      if (data.memes === undefined || data.memes.length == 0) {
+        console.log('NO DATA')
+      } else {
+        console.log({ DATA: data }, 'data available')
+      }
+    }
+  }, [isActive, data])
 
   return (
-    <div className={'relative mb-4 w-full md:mx-2 md:mb-0 md:w-1/4'}>
+    <div ref={searchContainerRef} className={'relative mb-4 w-full md:mx-2 md:mb-0 md:w-1/4'}>
       <div className={'flex flex-row'}>
         {/*        <label className="hidden" htmlFor="search-form">
           Search
         </label>*/}
         <input
-          ref={inputFieldRef}
           className="flex-grow-2 text-white border bg-custom-gray border-dotted stroke-dasharray: 6; focus:border-orange p-2 rounded-lg shadow-inner w-full"
           placeholder="Search"
           type="text"
-          onChange={(e) => {
+          /*onChange={(e) => {
             if (document.activeElement === inputFieldRef.current) {
               executeSearch({
                 variables: { filter: e.target.value },
               })
             }
+          }}*/
+          onChange={(e) => {
+            /*if (document.activeElement.contains(inputFieldRef.current)) {
+
+            }*/
+            setSearchFilter(e.target.value)
           }}
           onFocus={() => setIsActive(true)}
         />
+        <PrimaryBtn
+          mono={true}
+          onClick={(e) => {
+            e.preventDefault()
+            executeSearch({
+              variables: { filter: searchFilter },
+            })
+            setIsActive(true)
+          }}
+        >
+          Search
+        </PrimaryBtn>
       </div>
       <ul className={'absolute flex flex-col w-full rounded-xl bg-white mt-1'}>
         {data && isActive && <Dropdown data={data} />}
