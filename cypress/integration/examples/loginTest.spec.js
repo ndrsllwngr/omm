141fc28ba53a8fe44b71444f8897aa29102b683c
hawi <havyha@hotmail.com>
2021-02-28 13:15:03 +0100

    Add split tests

--- cypress/integration/examples/loginTest.spec.js
+++ cypress/integration/examples/loginTest.spec.js
@@ -6,13 +6,5 @@ describe('Cypress Demo-Login', () => {
     cy.get('#password').type('cypresstest123')
     cy.get('.border-transparent').click()
     cy.get('form').submit()
-    cy.get('.hover\\3A bg-gray-300').click()
-    cy.visit('https://omm.vercel.app/create')
-
-    cy.get('.flex:nth-child(3) > button > img').click()
-    cy.get('.upper-canvas').click()
-    cy.get('.col-span-9').click()
-    cy.get('.grid-cols-3').click()
-    cy.get('.flex-shrink-0:nth-child(8)').click()
   })
 })
